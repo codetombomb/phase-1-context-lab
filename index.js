06a20126d2158b531696ec1783c2fc589d67f02c
Tom Tobar <cocdetombomb@gmail.com>
2022-05-15 17:38:52 -0500

    add solution

--- index.js
+++ index.js
@@ -21,3 +21,58 @@ const allWagesFor = function () {
     return payable
 }
 
+function createEmployeeRecord(arr){
+    return {
+        firstName: arr[0],
+        familyName: arr[1],
+        title: arr[2],
+        payPerHour: arr[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+function createEmployeeRecords(arr){
+     return arr.map(employee => createEmployeeRecord(employee))
+}
+
+function createTimeInEvent(dateStamp){
+    this.timeInEvents.push(createTimeEvent(dateStamp,"TimeIn"))
+    return this
+}
+
+function createTimeOutEvent(dateStamp){
+    this.timeOutEvents.push(createTimeEvent(dateStamp,"TimeOut"))
+    return this
+}
+
+function hoursWorkedOnDate (date) {
+    let start = this.timeInEvents.find(record => record.date === date).hour
+    let end = this.timeOutEvents.find(record => record.date === date).hour
+    return (end - start) / 100
+}
+
+function wagesEarnedOnDate(date){
+    return hoursWorkedOnDate.call(this, date) * this.payPerHour
+}
+
+function findEmployeeByFirstName(srcArray, firstName){
+    return srcArray.find(record => record.firstName === firstName)
+}
+
+function calculatePayroll(arr){
+    return arr.reduce((acc, em) => {
+        return acc + allWagesFor.call(em)
+    },0)
+}
+
+// HELPERS
+function createTimeEvent(timeStamp, type){
+    return {
+        type,
+        hour: parseInt(timeStamp.split(" ")[1]),
+        date: timeStamp.split(" ")[0]
+    }
+}
+
+
